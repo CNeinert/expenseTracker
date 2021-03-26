da4300ce3aa1a8a4683b6f4ec7998e9ab30a1901
C. Neinert <54905826+CNeinert@users.noreply.github.com>
2021-03-26 21:09:51 +0100

    [remove] module-info.java since this is not a module

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module expenseTracker {
-    requires ormlite.core;
-    requires ormlite.jdbc;
-    requires java.sql;
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires junit;
-    exports main.java.run;
-}
