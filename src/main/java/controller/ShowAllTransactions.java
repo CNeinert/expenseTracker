fac5269aa903eecd465c5f1f1b794dfc6fd1fe4f
C. Neinert <54905826+CNeinert@users.noreply.github.com>
2022-01-16 21:37:28 +0100

    working table view

--- src/main/java/controller/ShowAllTransactions.java
+++ src/main/java/controller/ShowAllTransactions.java
@@ -1,30 +1,36 @@
 package main.java.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.scene.control.TreeTableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import main.java.model.Transaction;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class ShowAllTransactions extends AbstractViewController implements Initializable {
 
     @FXML
-    TreeTableView tbl_showAllTransactions;
+    TableView allTransactionsTable;
 
     @FXML
     Label lblTitle;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.tbl_showAllTransactions = new TreeTableView();
 
-        // programName.setCellValueFactory(new PropertyValueFactory<>("Program Name"));
-        // programVersion.setCellValueFactory(new PropertyValueFactory<>("Version"));
-        //initTableCols();
+
+        //programName.setCellValueFactory(new PropertyValueFactory<>("Program Name"));
+        //programVersion.setCellValueFactory(new PropertyValueFactory<>("Version"));
+        initTableCols();
     }
 
 
@@ -34,77 +40,61 @@ public class ShowAllTransactions extends AbstractViewController implements Initi
     }
 
     //TODO Make DRY!! or delete
-    /*
+    
     public void initTableCols() {
 
-        productObservableList.add(new Product());
-        TransactionTable.setEditable(true);
-        TableColumn product_name_col = new TableColumn("Produktbezeichnung");
-        product_name_col.setMinWidth(300);
-        product_name_col.setEditable(true);
-        product_name_col.setCellFactory(TextFieldTableCell.forTableColumn());
-        product_name_col.setCellValueFactory(new PropertyValueFactory<Product, String>("productName"));
-        product_name_col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Product, String>>() {
-            @Override
-            public void handle(TableColumn.CellEditEvent event) {
-                Product product = (Product) event.getRowValue();
-                product.setProductName(event.getNewValue().toString());
-                productObservableList.add(new Product());
-            }
+        ObservableList<Transaction> transactionObservableList = FXCollections.observableArrayList();
+        transactionObservableList.add(new Transaction());
+
+
+        TableColumn transaction_name_col = new TableColumn("Date");
+        transaction_name_col.setMinWidth(300);
+        transaction_name_col.setEditable(true);
+        transaction_name_col.setVisible(true);
+        transaction_name_col.setCellValueFactory(new PropertyValueFactory<Transaction, Date>("Date"));
+        transaction_name_col.setOnEditCommit((EventHandler<TableColumn.CellEditEvent<Transaction, Date>>) event -> {
+            Transaction transaction = (Transaction) event.getRowValue();
+            transaction.setDate((Date) event.getNewValue());
+            transactionObservableList.add(new Transaction());
         });
+        allTransactionsTable.getColumns().addAll(transaction_name_col);
+        allTransactionsTable.setVisible(true);
+//
+//
+        //TableColumn amount_col = new TableColumn("Preis");
+        //amount_col.setMinWidth(100);
+        //amount_col.setEditable(true);
+        //amount_col.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));//TODO Handle Exeption
+        //amount_col.setCellValueFactory(new PropertyValueFactory<Transaction, Double>("amount"));
+        //amount_col.setOnEditCommit((EventHandler<TableColumn.CellEditEvent>) cellEditEvent -> {
+        //    Transaction thisTransaction = transactionObservableList.get(transactionObservableList.size()-2);
+        //    thisTransaction.setAmount((Double) cellEditEvent.getNewValue());
+        //    allTransactionsTable.refresh();
+        //});
+//
+//
+        //TableColumn notes_col = new TableColumn("Notizen");
+        //notes_col.setMinWidth(200);
+        //notes_col.setEditable(true);
+        //notes_col.setCellFactory(TextFieldTableCell.forTableColumn()); //TODO Handle Exeption
+        //notes_col.setCellValueFactory(new PropertyValueFactory<Transaction, String>("notes"));
+        //notes_col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Transaction, String>>() {
+        //    @Override
+        //    public void handle(TableColumn.CellEditEvent event) {
+        //        Transaction thisTransaction = transactionObservableList.get(transactionObservableList.size()-2);
+        //        thisTransaction.setNotes(event.getNewValue().toString());
+        //        allTransactionsTable.refresh();
+        //    }
+        //});
+//
+        //allTransactionsTable.setItems(transactionObservableList);
+        ////allTransactionsTable.getColumns().removeAll();
+//
+        //allTransactionsTable.getColumns().addAll(transaction_name_col, amount_col,notes_col );
+        //allTransactionsTable.getRowFactory();
+        System.out.println(allTransactionsTable.getColumns().size());
 
-        TableColumn single_price_col = new TableColumn("Einzelpreis");
-        single_price_col.setMinWidth(100);
-        single_price_col.setEditable(true);
-        single_price_col.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));//TODO Handle Exeption
-        single_price_col.setCellValueFactory(new PropertyValueFactory<Product, Double>("single_price"));
-        single_price_col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent>() {
-            @Override
-            public void handle(TableColumn.CellEditEvent cellEditEvent) {
-                Product thisProduct = productObservableList.get(productObservableList.size()-2);
-                thisProduct.setSingle_price((Double) cellEditEvent.getNewValue());
-            }
-        });
-
-
-        TableColumn amount_col = new TableColumn("Menge");
-        amount_col.setMinWidth(100);
-        amount_col.setEditable(true);
-        amount_col.setCellFactory(TextFieldTableCell.forTableColumn(new DoubleStringConverter()));//TODO Handle Exeption
-        amount_col.setCellValueFactory(new PropertyValueFactory<Product, Double>("amount"));
-        amount_col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent>() {
-            @Override
-            public void handle(TableColumn.CellEditEvent cellEditEvent) {
-                Product thisProduct = productObservableList.get(productObservableList.size()-2);
-                thisProduct.setAmount((Double) cellEditEvent.getNewValue());
-                TransactionTable.refresh();
-            }
-        });
-
-
-        TableColumn notes_col = new TableColumn("Notizen");
-        notes_col.setMinWidth(200);
-        notes_col.setEditable(true);
-        notes_col.setCellFactory(TextFieldTableCell.forTableColumn()); //TODO Handle Exeption
-        notes_col.setCellValueFactory(new PropertyValueFactory<Product, String>("notes"));
-        notes_col.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<Product, String>>() {
-            @Override
-            public void handle(TableColumn.CellEditEvent event) {
-                Product thisProduct = productObservableList.get(productObservableList.size()-2);
-                thisProduct.setNotes(event.getNewValue().toString());
-                TransactionTable.refresh();
-            }
-        });
-
-
-
-
-        TransactionTable.setItems(productObservableList);
-        //TransactionTable.getColumns().removeAll();
-
-        TransactionTable.getColumns().addAll(product_name_col, single_price_col, amount_col,notes_col );
-        TransactionTable.getRowFactory();
         System.out.println("Init Table");
     }
-    */
+    
 }
